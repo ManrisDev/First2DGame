52c0a611d097be5a58e102fa21c92f213813a00f
Manris <polesko02@mail.ru>
2022-11-04 23:47:39 +0300

    Add character sprites & change map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    private Vector3 position;
+    [SerializeField]
+    [Range(0f, 10f)] private float camSpeed = 2f;
+    [SerializeField]
+    [Range(0f, 10f)] private float camHeight = 2.5f;
 
-    private void Awake()
-    {
-        if (!player)
-            player = FindObjectOfType<Hero>().transform;
-    }
+    private Vector3 position;
 
     private void Update()
     {
         position = player.position;
         position.z = -10f;  //„тобы камера не приближалась к игроку
+        position.y += camHeight;
 
         //ѕлавное перемещение камеры за игроком
-        transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, position, camSpeed * Time.deltaTime);
     }
 
 }
